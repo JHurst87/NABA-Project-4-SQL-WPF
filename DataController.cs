7d721b88064b0ea82445031601f8ad099b19f75a
JHurst87 <72364767+JHurst87@users.noreply.github.com>
2020-10-15 14:17:52 -0500

    Add files via upload

---
+++ DataController.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace DataController
+{
+    class DataControl
+    {
+        const string WRITE_CMD = "INSERT INTO NABA.dbo.Person ([FirstName], [LastName]) VALUES (@firstName, @lastName)";
+        const string DELETE_CMD = "DELETE FROM NABA.dbo.Person";
+        SqlConnection connectionString = new SqlConnection(@"Data Source = DESKTOP-GQR2API;Initial Catalog = NABA;Trusted_Connection = true");
+
+        public void ToDatabase(string fName, string lName)
+        {
+            using SqlCommand cmd = new SqlCommand(WRITE_CMD, connectionString);
+
+            connectionString.Open();
+
+            cmd.Parameters.Add("@firstName", System.Data.SqlDbType.NChar).Value = fName;
+
+            cmd.Parameters.Add("@lastName", System.Data.SqlDbType.NChar).Value = lName;
+
+            cmd.ExecuteNonQuery();
+            connectionString.Close();
+        }
+        public void DeleteData()
+        {
+            connectionString.Open();
+            SqlCommand cmd = new SqlCommand(DELETE_CMD, connectionString);
+            cmd.ExecuteNonQuery();
+            connectionString.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿//Created by: Jordan Hurst
+//Date: October 14, 2020
+//Program takes in input from textboxes  when the Subumit button is clicked and saves them to an SQL database
+//Program displays the SQL database file on the screen when the Print button is clicked.
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
+using DataController;
+
+namespace PersonInfoSQL
+{    
+    public partial class MainWindow : Window
+    {        
+        DataControl data = new DataControl();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstName.Text;
+            string lastName = LastName.Text;
+            data.ToDatabase(firstName, lastName);
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            const string READ_CMD = "SELECT * FROM NABA.dbo.Person";
+            SqlConnection connectionString = new SqlConnection(@"Data Source = DESKTOP-GQR2API;Initial Catalog = NABA;Trusted_Connection = true");
+            using SqlCommand read = new SqlCommand(READ_CMD, connectionString);
+            SqlDataAdapter sda = new SqlDataAdapter(read);
+            DataTable dt = new DataTable("Person Info");
+            sda.Fill(dt);
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void DeleteAll_Click(object sender, RoutedEventArgs e)
+        {
+            data.DeleteData();
+        }
+    }
+}
